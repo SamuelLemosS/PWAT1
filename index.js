10795e85d2710fee4a9c87390e6f605ef5e28c8d
SamuelLemosS <107142956+SamuelLemosS@users.noreply.github.com>
2022-09-08 21:20:09 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+const digitar = document.getElementById("digitar");
+const bt = document.getElementById("bt");
+const value = document.getElementById("value");
+
+const click = () => {
+    var vetor=[];
+    var tarefa=digitar.value;
+    vetor.push(tarefa);
+    for(i=0;i<vetor.length;i++){
+        value.innerHTML += "<li>Nome Digitado = "+vetor[i]+ "<br /></li>";
+    }
+}
+
+bt.onclick = click;
